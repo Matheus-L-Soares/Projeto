d64b5be27dc0156f93c8c643c35b2a30d5a48f72
Matheus Lisboa <matheuslisboa86@hotmail.com>
2023-03-26 14:13:54 -0300

    'Criado exemplos dos tipos básicos dos comados em Python'

---
+++ Tipo float.py
@@ -0,0 +1,29 @@
+print(type(1.1))
+print(1.1)
+
+print(type(0.34))
+print(0.34)
+
+print(type(.1 + .2)) # Esse formato é utilizado para dar exatidão o resultado. Ex: trabalhar com moedas
+print(.1 + .2)
+
+print(type(.1 * .2))
+print(.1 * .2)
+
+print(type(.1 - .2))
+print(.1 - .2)
+
+print(type(.1 / .2))
+print(.1 / .2)
+
+print(int(1.5))
+
+print(float(1))
+
+print(type(1j))
+
+print(type(1j ** 2))
+print(1j ** 2)
+
+print(type((1 + 1j)**4))
+print((1 + 1j)**4)
--- Tipos Básicos/Tipo Inteiro.py
+++ Tipos Básicos/Tipo Inteiro.py
@@ -1,3 +1,24 @@
 r = 1 + 1
 print(type(r))
 print(r)
+
+r_1 = 5//2
+print(type(r_1))
+print(r_1)
+
+r_2 = 5 % 2
+print(type(r_2))
+print(r_2)
+
+r_3 = 2**3
+print(type(r_3))
+print(r_3)
+
+r_4 = 2**63
+print(type(r_4))
+print(r_4)
+
+r_5 = 2**1000
+print(type(r_5))
+print(r_5)
+
